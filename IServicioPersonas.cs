89856ec466574ed0729aeac8915b0a73e42222a4
joelqwerty <65176246+joelqwerty@users.noreply.github.com>
2022-03-02 11:45:43 -0600

    Add files via upload

---
+++ IServicioPersonas.cs
@@ -0,0 +1,35 @@
+﻿using EntityPersonas2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebWCFPersonas
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioPersonas" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioPersonas
+    {
+        [OperationContract]
+        List<EntPersonas> Obtener();
+
+
+        [OperationContract]
+        EntPersonas ObtenerPorId(int Id);
+
+
+        [OperationContract] 
+        void Agregar(EntPersonas x);
+
+
+        [OperationContract]
+        void Editar(EntPersonas x);
+
+
+        [OperationContract]
+        void Eliminar(EntPersonas x);
+
+    }
+}
---
+++ ServicioPersonas.svc.cs
@@ -0,0 +1,51 @@
+﻿using DataBussinesPersonas;
+using EntityPersonas2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebWCFPersonas
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioPersonas" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioPersonas.svc o ServicioPersonas.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioPersonas : IServicioPersonas
+    {
+        public List<EntPersonas> Obtener()
+        {
+            DBPersonas datos = new DBPersonas();
+            List<EntPersonas> ls = datos.Obtener();
+            return ls;
+        }
+
+        public EntPersonas ObtenerPorId(int Id)
+        {
+            DBPersonas datos = new DBPersonas();
+            EntPersonas persona = datos.Obtener(Id);
+            return persona;
+        }
+
+        public void Agregar(EntPersonas x)
+        {
+            DBPersonas datos = new DBPersonas();
+            datos.Agregar(x);
+        }
+
+        public void Editar(EntPersonas x)
+        {
+            DBPersonas datos = new DBPersonas();
+            datos.Editar(x);
+        }
+
+        public void Eliminar(EntPersonas x)
+        {
+            DBPersonas datos = new DBPersonas();
+            datos.Eliminar(x);
+        }
+
+    }
+
+}
+
